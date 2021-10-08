eed06a2d8adeed547d97899d339250986f9fa307
vishal <vishalrathod7038@gmail.com>
2021-10-08 23:08:28 +0530

    UC1 Assignment completed

---
+++ UC1Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC1Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Is_Present = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if (Is_Present == empInput)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine(" Employee is Absent");
+            }
+        }
+    }
+}
