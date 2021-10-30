c782b6d01331810e3b57856cc6460740dd2ee473
vishal <vishalrathod7038@gmail.com>
2021-10-30 21:44:21 +0530

    Emp Wages compute Uc13 is done

---
+++ UC13empWagesAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC13empWagesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an array class object
+            //calling all the necessary methods of different classes
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 4, 7, 30);
+            empWageBuilder.addCompanyEmpWage("Reliance", 5, 30, 25);
+            empWageBuilder.addCompanyEmpWage("Ford", 5, 20, 35);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
