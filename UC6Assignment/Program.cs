5337b8280129e4b5ef95a6c70915c469d06f3953
vishal <vishalrathod7038@gmail.com>
2021-10-30 21:22:35 +0530

    Emp Wages Compute Uc6 Assignment

---
+++ UC6Assignment/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace UC6Assignment
+{
+    class Program
+    { 
+       public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int Emp_Rate_Per_Hour = 20;
+        public const int NUM_WORKING_DAY = 2;
+        public const int MAX_HOUR_IN_MONTH = 10;
+        static void Main(string[] args)
+        {
+            int totalemphour = 0;
+            int emphrs = 0;
+            
+            int totalW0rkingDay = 0;
+            int empwage = 0;
+           while (totalemphour<= MAX_HOUR_IN_MONTH  && totalW0rkingDay< NUM_WORKING_DAY)
+            {
+
+                totalW0rkingDay++;
+                // creating obj of Random Predefine class
+                Random random = new Random();
+                //using Next() to generaret random input out of 0,1
+                int empcheck = random.Next(0, 3);
+
+                switch (empcheck)
+                {
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Employee is FULL Time Present");
+                        emphrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is Part Time Present");
+
+                        emphrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        emphrs = 0;
+                        break;
+
+                }
+               
+                totalW0rkingDay += emphrs;
+                Console.WriteLine("Days#:"+ totalW0rkingDay + "Emp hrs:"+emphrs);
+            }
+
+            empwage = emphrs * Emp_Rate_Per_Hour;
+            empwage += emphrs;
+            Console.WriteLine("empwagec:" + empwage);
+
+            /*int totalEmpWage = totalemphour * Emp_Rate_Per_Hour;
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);*/
+            Console.ReadLine();
+        }
+    }
+}
